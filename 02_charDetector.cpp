108ee2edaf5df6c64d9c4d2f695d7f72523cd076
yeasinarafatprantik2002 <yeasinarafatprantik@gmail.com>
2023-10-12 14:05:13 +0600

    refine chardetector logic

--- 02_charDetector.cpp
+++ 02_charDetector.cpp
@@ -5,8 +5,9 @@ int main()
 {
     char chr;
     cout << "Enter Your Character: ";
+    cin >> chr;
 
-    if (cin >> chr && chr == 'A' || chr == 'a' || chr == 'E' || chr == 'e' || chr == 'I' || chr == 'i' || chr == 'O' || chr == 'o' || chr == 'U' || chr == 'u' || chr == 'B' || chr == 'b' || chr == 'C' || chr == 'c' || chr == 'D' || chr == 'd' || chr == 'F' || chr == 'f' || chr == 'G' || chr == 'g' || chr == 'H' || chr == 'h' || chr == 'J' || chr == 'j' || chr == 'K' || chr == 'k' || chr == 'L' || chr == 'l' || chr == 'M' || chr == 'm' || chr == 'N' || chr == 'n' || chr == 'P' || chr == 'p' || chr == 'Q' || chr == 'q' || chr == 'R' || chr == 'r' || chr == 'S' || chr == 's' || chr == 'T' || chr == 't' || chr == 'V' || chr == 'v' || chr == 'W' || chr == 'w' || chr == 'X' || chr == 'x' || chr == 'Y' || chr == 'y' || chr == 'Z' || chr == 'z')// Check char datatypes
+    if (chr == chr > 'A' || chr == chr > 'a' && chr == chr < 'Z' || chr == chr < 'z') // Check char datatypes
 
     {
 
