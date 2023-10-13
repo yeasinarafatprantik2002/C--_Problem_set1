6163317b1fe0e44bde079f77ae6671a6a3bdf182
yeasinarafatprantik2002 <yeasinarafatprantik@gmail.com>
2023-10-13 12:50:59 +0600

    add largestNumber.cpp

---
+++ 06_largestNum.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int num1;
+    cout << "Enter First Number: ";
+
+    if (cin >> num1)
+    {
+        int num2, num3;
+        cout << "Enter Second Number: ";
+        cin >> num2;
+        cout << "Enter Third Number: ";
+        cin >> num3;
+        if (num1 > num2 && num1 > num3)
+        {
+            cout << "The Largest Number Is: " << num1;
+        }
+        else if (num2 > num1 && num2 > num3)
+        {
+            cout << "The Largest Number Is: " << num2;
+        }
+        else if (num3 > num1 && num3 > num2)
+        {
+            cout << "The Largest Number Is: " << num3;
+        }
+        else
+        {
+            cout << "All Numbers Are Equal!";
+        }
+    }
+    else
+    {
+        cout << "Please Enter A Valid Number!";
+    }
+
+    return 0;
+}
